15c8623c16ad0a595a81eeb5807ad5917f6eab59
Kunal J. Pardeshi <kunaljp16@gmail.com>
2022-03-18 12:23:31 +0100

    added arrow component with test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,16 @@
 import { shallow } from 'enzyme';
-import App from "../App";
+import App from "../app";
 
 describe("app component", () => {
   it("should render h1", () => {
     const wrapper = shallow(<App />);
     const heading = wrapper.find('h1');
     expect(heading.length).toBe(1);
+  });
+  it("should render button", () => {
+    const wrapper = shallow(<App />);
+    const button = wrapper.find('button');
+    expect(button.length).toBe(1);
+    expect(button.text()).toEqual('SORT GAMES');
   });
 });
---
+++ src/__tests__/arrow.test.js
@@ -0,0 +1,38 @@
+import { shallow, mount } from "enzyme";
+import Arrow from "../components/sharedUI/arrows/arrow";
+
+describe("Arrow component", () => {
+  it("should render i element", () => {
+    const wrapper = shallow(<Arrow />);
+    const arrow = wrapper.find("i");
+    expect(arrow.length).toBe(1);
+  });
+
+  it("should render i element with direction", () => {
+
+    const direction = {
+      up: "up",
+      down: "down",
+      right: "right",
+      left: "left",
+    };
+
+    const wrapper = shallow(<Arrow className={"arrow " + direction} />);
+    if (direction === direction.up) {
+      expect(wrapper.hasClass("up")).toBeTruthy();
+    }
+
+    if (direction === direction.down) {
+      expect(wrapper.hasClass("down")).toBeTruthy();
+    }
+
+    if (direction === direction.right) {
+      expect(wrapper.hasClass("right")).toBeTruthy();
+    }
+
+    if (direction === direction.left) {
+      expect(wrapper.hasClass("left")).toBeTruthy();
+    }
+
+  });
+});
