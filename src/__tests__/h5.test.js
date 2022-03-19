fa88eb09f02039e301b972eedff08665cbdc1c53
Kunal J. Pardeshi <kunaljp16@gmail.com>
2022-03-19 14:08:54 +0100

    h5 component added

---
+++ src/__tests__/h5.test.js
@@ -0,0 +1,8 @@
+import { shallow } from "enzyme";
+import H5 from "../../../../atomicDesignComponents/Atoms/h5/h5";
+
+describe("h5", () => {
+  it("shouldRenderH5", () => {
+    shallow(<H5 />);
+  });
+});
---
+++ src/atomicDesignComponents/atoms/h5/h5.js
@@ -0,0 +1,7 @@
+import "./h5.scss";
+
+function H5(props) {
+  return <h5>{props.text}</h5>;
+}
+
+export default H5;
