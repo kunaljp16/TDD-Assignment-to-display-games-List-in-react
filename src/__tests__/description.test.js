788337057620b3fa57719a467b50d64d79c432b3
Kunal J. Pardeshi <kunaljp16@gmail.com>
2022-03-23 00:22:48 +0100

    test for description and header updated

--- src/__tests__/description.test.js
+++ src/__tests__/description.test.js
@@ -4,12 +4,10 @@ import Description from "./../atomicDesignComponents/organisms/description/descr
 describe("Description", () => {
   it("should Render Description", () => {
     const wrapper = shallow(<Description />);
-    console.log(wrapper.debug());
-
     const itemImage = wrapper.find("ItemImage");
     expect(itemImage).toHaveLength(1);
 
-    const description = wrapper.find("Description");
-    expect(description).toHaveLength(1);
+    const itemTextInfo = wrapper.find("ItemTextInfo");
+    expect(itemTextInfo).toHaveLength(1);
   });
 });
--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,11 +1,9 @@
 import { shallow } from "enzyme";
 import Header from "./../atomicDesignComponents/molecules/header/header";
-import H4 from "./../atomicDesignComponents/atoms/h4/h4";
 
 describe("header", () => {
   it("should render header ", () => {
     const wrapper = shallow(<Header />);
-    console.log(wrapper.debug());
     const h4 = wrapper.find("H4");
     expect(h4).toHaveLength(1);
 
